f19c5c0cfd67dd92cd8487d19d89036157b405db
HieuVV <voxvanhieu@gmail.com>
2024-05-07 01:37:07 +0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import App from './components/App';
+import { store } from './data/store';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
